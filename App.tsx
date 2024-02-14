6f8c0fa6111b37635fdce63059486042607c2b6f
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-14 15:08:16 +0700

    Adjust Code

--- App.tsx
+++ App.tsx
@@ -3,6 +3,11 @@ import { View, Text, Image, ActivityIndicator, Dimensions } from 'react-native'
 //Dimensions Calculate Display size
 import React from 'react'
 
+const activityXPosition = Dimensions.get('screen').width/2 - 25;
+const activityYPosition = Dimensions.get('screen').height/2 - 25;
+{/* Dimensions.get('screen').width/2 - 25 => get width size of screen divide 2 for get half of width and minus 2 (radius)  */}
+
+
 const App = () => {
   return (
     <View style={{ flex: 1, backgroundColor: '#FF0', flexDirection: 'column'}}>
@@ -11,8 +16,7 @@ const App = () => {
       <Text style={{ backgroundColor: '#10F', flex:1 }}>Codemobile 3#</Text>
       <Text style={{ backgroundColor: '#80F', flex:1 }}>Codemobile 4#</Text>
       <Image style={{ width:null, flex:1}} resizeMode='contain'  source={require("../Major/static/image/major.png")}></Image>
-      <ActivityIndicator color="white" style={{ position:"absolute", left: Dimensions.get('screen').width/2 - 25, top : Dimensions.get('screen').height/2-25  }} size={50}/>
-      {/* Dimensions.get('screen').width/2 - 25 => get width size of screen divide 2 for get half of width and minus 2 (radius)  */}
+      <ActivityIndicator color="white" style={{ position:"absolute", left: activityXPosition, top : activityYPosition }} size={50}/>
     </View>
   )
 }
