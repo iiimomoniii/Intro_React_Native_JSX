8cf9d0c5c9b4a8a7be88930670643beeee44dc9b
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-14 15:31:33 +0700

    Update
    Button and Style for Button Position

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button, Alert } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="ClickMe" onPress={() => {
+        Alert.alert("Hey");
+      }}></Button>
+      <View style={{ marginTop: 20 }}>
+        <Button title="ClickMe" onPress={() => {
+          Alert.alert("Hey");
+        }}></Button>
+      </View>
+
     </View>
   )
 }
