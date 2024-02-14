8b21f8a5dde35c094bae8dc054798f2d494ff82e
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-14 14:30:34 +0700

    image

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: '#FF0', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly'}}>
-      <Text style={{ backgroundColor: '#F00',width: 100 }}>Codemobile 1#</Text>
-      <Text style={{ backgroundColor: '#F0F',width: 200 }}>Codemobile 2#</Text>
-      <Text style={{ backgroundColor: '#10F',width: 300 }}>Codemobile 3#</Text>
-      <Text style={{ backgroundColor: '#80F',width: 400 }}>Codemobile 4#</Text>
+    <View style={{ flex: 1, backgroundColor: '#FF0', flexDirection: 'column'}}>
+      <Text style={{ backgroundColor: '#F00', flex:1 }}>Codemobile 1#</Text>
+      <Text style={{ backgroundColor: '#F0F', flex:1 }}>Codemobile 2#</Text>
+      <Text style={{ backgroundColor: '#10F', flex:1 }}>Codemobile 3#</Text>
+      <Text style={{ backgroundColor: '#80F', flex:1 }}>Codemobile 4#</Text>
+      <Image style={{ width:null, flex:1}} resizeMode='contain'  source={require("../Major/static/image/major.png")}></Image>
     </View>
   )
 }
