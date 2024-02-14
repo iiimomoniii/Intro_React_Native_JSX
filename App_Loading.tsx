4ab386a4e8a8059cd8bfe86602d87617496856c1
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-14 15:11:45 +0700

    Adjust Files Name

---
+++ App_Loading.tsx
@@ -0,0 +1,24 @@
+import { View, Text, Image, ActivityIndicator, Dimensions } from 'react-native'
+//ActivityIndicator (Loading)
+//Dimensions Calculate Display size
+import React from 'react'
+
+const activityXPosition = Dimensions.get('screen').width/2 - 25;
+const activityYPosition = Dimensions.get('screen').height/2 - 25;
+{/* Dimensions.get('screen').width/2 - 25 => get width size of screen divide 2 for get half of width and minus 2 (radius)  */}
+
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: '#FF0', flexDirection: 'column'}}>
+      <Text style={{ backgroundColor: '#F00', flex:1 }}>Codemobile 1#</Text>
+      <Text style={{ backgroundColor: '#F0F', flex:1 }}>Codemobile 2#</Text>
+      <Text style={{ backgroundColor: '#10F', flex:1 }}>Codemobile 3#</Text>
+      <Text style={{ backgroundColor: '#80F', flex:1 }}>Codemobile 4#</Text>
+      <Image style={{ width:null, flex:1}} resizeMode='contain'  source={require("../Major/static/image/major.png")}></Image>
+      <ActivityIndicator color="white" style={{ position:"absolute", left: activityXPosition, top : activityYPosition }} size={50}/>
+    </View>
+  )
+}
+
+export default App
