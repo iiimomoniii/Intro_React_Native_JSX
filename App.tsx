876719b4b976ab3a3c708ca32b084f0db951ed64
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-14 15:47:54 +0700

    Update
    Button and Text alert

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import { View, Text, Button, Alert } from 'react-native'
+import { View, Text, Button, Alert, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="ClickMe" onPress={() => {
-        Alert.alert("Hey");
-      }}></Button>
+
       <View style={{ marginTop: 20 }}>
-        <Button title="ClickMe" onPress={() => {
-          Alert.alert("Hey");
-        }}></Button>
+
+        <TouchableOpacity onPress={() => { Alert.alert("Hello") }}>
+          <Text >TextButton</Text>
+        </TouchableOpacity>
+
       </View>
 
     </View>
