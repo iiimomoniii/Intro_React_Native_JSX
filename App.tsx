c9d8300875db4334c052098b6652fb07f0a94733
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-16 08:51:39 +0700

    Update
    add hello world

--- App.tsx
+++ App.tsx
@@ -4,20 +4,15 @@ import React from 'react'
 const App = () => {
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-
-      <View style={{ marginTop: 20 }}>
-
-        <TouchableOpacity
-          style={{ marginTop: 24, borderColor: "#3b5998", borderWidth: 1, padding: 24, borderRadius: 5, backgroundColor: "#3b5998" }} 
-          onPress={() => { Alert.alert("Hello") }}>
-          <Text style={{ color: '#ffffff' }}>TextButton</Text>
-        </TouchableOpacity>
-
-      </View>
-
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, world!</Text>
     </View>
-  )
+  );
 }
 
 export default App
